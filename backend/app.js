5e86097a7cd3fd6b4ccd2d9940c81bc373dfa436
jorgemacias <jjmaciasd@guanajuato.gob.mx>
2018-10-01 15:24:56 -0500

    Se agrega variable de session en app.js

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,11 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 var mongoose = require('mongoose');
+
+var session = require('express-session')
+
+// Se crea la sesion del cliente
+app.use(session({ secret: 'pablitoclavounclavitoenlacalvadeuncalvito', cookie: { maxAge: 60000 }}))
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'twig');
